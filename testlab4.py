9494e2d6d238a5c06dc9097bc0170130a9c9d3d3
Vuxvuz <148697176+Vuxvuz@users.noreply.github.com>
2024-05-13 15:20:17 +0700

    Create testlab4.py

---
+++ testlab4.py
@@ -0,0 +1,24 @@
+import http.client
+from urllib.parse import urlencode
+import time
+key = "2QACR5MRWUQ9JY6Z"  # Put your API Key here
+def thermometer():
+    while True:
+        #Calculate CPU temperature of Raspberry Pi in Degrees C
+        temp = int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3 # Get Raspberry Pi CPU temp
+        params = urlencode({'field1': temp, 'key':key }) 
+        headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+        conn = http.client.HttpConnection("api.thingspeak.com:80")
+        try:
+            conn.request("POST", "/update", params, headers)
+            response = conn.getresponse()
+            print temp
+            print response.status, response.reason
+            data = response.read()
+            conn.close()
+        except:
+            print "connection failed"
+        break
+if __name__ == "__main__":
+        while True:
+                thermometer()
